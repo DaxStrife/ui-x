4f4edf75a02086cffc9f6faf8a6fa0d2bd8e7a9e
DaxStrife <hector.rendon.b@gmail.com>
2024-12-04 19:09:03 -0600

    init project

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // Configura @ para apuntar a la carpeta src
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
