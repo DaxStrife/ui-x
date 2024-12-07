9d641bbafad9bb11f7939506e70ee93d26a76272
DaxStrife <hector.rendon.b@gmail.com>
2024-12-07 10:31:37 -0600

    i was workin in tooltip

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ import "../index.css";
 
 export { default as Button } from "./button/Main.jsx";
 export { default as Modal } from "./modal/Main.jsx";
+export { default as Tooltip } from "./tooltip/Main.jsx";
---
+++ src/components/tooltip/Main.jsx
@@ -0,0 +1,28 @@
+import cn from "../../utils/cn";
+
+const Main = ({ children, content, className = "", dir = "top", ...props }) => {
+  const positions = {
+    top: "left-1/2 -translate-x-1/2 bottom-full mb-2",
+    right: "left-full top-1/2 -translate-y-1/2 ml-2",
+    left: "right-full top-1/2 -translate-y-1/2 mr-2",
+    bottom: "left-1/2 -translate-x-1/2 top-full mt-2",
+  };
+
+  const positionClass = positions[dir] || positions.top;
+
+  return (
+    <div {...props} className="group relative max-w-max">
+      {children}
+      <div
+        role="tooltip"
+        className={cn(
+          `absolute w-full z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-800 rounded-lg shadow-sm opacity-0 group-hover:opacity-100 group-hover:visible group-hover:scale-100 ${positionClass} ${className}`
+        )}
+      >
+        {content}
+      </div>
+    </div>
+  );
+};
+
+export default Main;
