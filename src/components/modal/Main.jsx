d47295869921380b12d6a859760e8940bc52fa7f
DaxStrife <hector.rendon.b@gmail.com>
2024-12-07 18:56:56 -0600

    remove px in z index

--- src/components/modal/Main.jsx
+++ src/components/modal/Main.jsx
@@ -1,7 +1,7 @@
 const Main = ({ open = false, setOpen = () => {}, children }) => {
   if (!open) return null;
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-[9999px]">
+    <div className="fixed inset-0 flex items-center justify-center z-[9999]">
       <div onClick={() => setOpen(false)} className="fixed inset-0 bg-black/50 cursor-pointer" />
       <div className="bg-white rounded-lg shadow-lg mx-2 p-4 md:p-6 max-w-md md:max-w-4xl w-full relative">
         {children}
