e026846326d05c40a4e56c95d922c822b3ac36b8
DaxStrife <hector.rendon.b@gmail.com>
2024-12-16 14:01:35 -0600

    add svg as jsx

---
+++ src/icons/Ic_arrow.jsx
@@ -0,0 +1,26 @@
+export const Ic_arrow_left = ({ ...props }) => (
+  <svg width={24} height={24} xmlns="http://www.w3.org/2000/svg" className="fill-current" {...props}>
+    <path d="M16.2375 21.4875C16.0125 21.4875 15.7875 21.4125 15.6375 21.225L7.16249 12.6C6.82499 12.2625 6.82499 11.7375 7.16249 11.4L15.6375 2.77498C15.975 2.43748 16.5 2.43748 16.8375 2.77498C17.175 3.11248 17.175 3.63748 16.8375 3.97498L8.96249 12L16.875 20.025C17.2125 20.3625 17.2125 20.8875 16.875 21.225C16.65 21.375 16.4625 21.4875 16.2375 21.4875Z" />
+  </svg>
+);
+
+export const Ic_arrow_right = ({ ...props }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} className="fill-current" {...props}>
+    <path d="M7.763 21.488a.92.92 0 0 1-.6-.226.838.838 0 0 1 0-1.2L15.037 12 7.162 3.975a.838.838 0 0 1 0-1.2.838.838 0 0 1 1.2 0l8.475 8.625a.838.838 0 0 1 0 1.2l-8.474 8.625a.885.885 0 0 1-.6.262Z" />
+  </svg>
+);
+
+export const Ic_select = ({ ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={24}
+    height={24}
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={2}
+    {...props}
+  >
+    <path d="m7 15 5 5 5-5M7 9l5-5 5 5" />
+  </svg>
+);
---
+++ src/icons/Ic_eye.jsx
@@ -0,0 +1,39 @@
+export const Ic_eye = ({ ...props }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      fill="none"
+      stroke="#0f0f0f"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-eye"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M10 12a2 2 0 1 0 4 0 2 2 0 0 0-4 0" />
+      <path d="M21 12c-2.4 4-5.4 6-9 6-3.6 0-6.6-2-9-6 2.4-4 5.4-6 9-6 3.6 0 6.6 2 9 6" />
+    </svg>
+  );
+};
+
+export const Ic_eye_off = ({ ...props }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-eye-off"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M10.585 10.587a2 2 0 0 0 2.829 2.828" />
+      <path d="M16.681 16.673A8.717 8.717 0 0 1 12 18c-3.6 0-6.6-2-9-6 1.272-2.12 2.712-3.678 4.32-4.674m2.86-1.146A9.055 9.055 0 0 1 12 6c3.6 0 6.6 2 9 6-.666 1.11-1.379 2.067-2.138 2.87M3 3l18 18" />
+    </svg>
+  );
+};
