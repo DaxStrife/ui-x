db2f4d30e6d06cbe00727bff5e878749bca20f49
DaxStrife <hector.rendon.b@gmail.com>
2024-12-18 20:58:34 -0600

    i worked loading spinner

---
+++ src/components/spinner/Main.jsx
@@ -0,0 +1,17 @@
+import cn from "@/utils/cn";
+
+const Main = ({ className }) => {
+  return (
+    <div
+      className={cn(
+        `animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full ${className}`
+      )}
+      role="status"
+      aria-label="loading"
+    >
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+};
+
+export default Main;
