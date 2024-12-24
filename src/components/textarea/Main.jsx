8f32f285f0aa9a8701c09bbdb5bb7e7a704b3e2a
DaxStrife <hector.rendon.b@gmail.com>
2024-12-24 16:30:12 -0600

    im workin on a text area

---
+++ src/components/textarea/Main.jsx
@@ -0,0 +1,16 @@
+import cn from "@/utils/cn";
+
+const Main = ({ placeholder = "", className = "", ...props }) => {
+  return (
+    <textarea
+      {...props}
+      placeholder={placeholder}
+      // style={{ fieldSizing: "content" }}
+      className={cn(
+        `py-2 px-4 w-56 rounded-lg ring-slate-200 transition-all placeholder:text-slate-500 focus:ring-[3px] bg-slate-100 [&::-webkit-scrollbar]:w-[6px] [&::-webkit-scrollbar-thumb]:bg-slate-600/50 [&::-webkit-scrollbar-thumb]:rounded-md ${className}`
+      )}
+    />
+  );
+};
+
+export default Main;
