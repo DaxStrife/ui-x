a13e09bbf395cedb43503fdf43fac013b82d73dd
DaxStrife <hector.rendon.b@gmail.com>
2025-01-01 11:48:45 -0600

    i chane the style of awitch

--- src/components/switch/Main.jsx
+++ src/components/switch/Main.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import cn from "@/utils/cn";
 
 const Main = ({ value, setValue, className = "", ...props }) => {
@@ -9,7 +7,7 @@ const Main = ({ value, setValue, className = "", ...props }) => {
       <div
         onClick={() => setValue(!value)}
         className={cn(
-          `relative w-12 h-6 after:h-5 after:w-5 bg-slate-100 peer-focus:ring-blue-200 peer-checked:bg-blue-700 after:bg-white after:border-slate-400 after:start-[4px] peer-focus:outline-none peer-focus:ring-4 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:border after:rounded-full after:transition-all ${className}`
+          `relative w-10 h-4 after:h-5 after:w-5 bg-slate-100 peer-focus:ring-blue-200 peer-checked:bg-blue-700 after:bg-white after:border-slate-400 after:start-[0px] peer-focus:outline-none peer-focus:ring-4 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute  after:top-1/2 after:-translate-y-1/2 after:border after:rounded-full after:transition-all ${className}`
         )}
       />
     </div>
