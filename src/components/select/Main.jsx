d2ce7b1dae2108fd4925b421e3646d2068feae1e
DaxStrife <hector.rendon.b@gmail.com>
2025-01-01 11:51:02 -0600

    i was working on the select

---
+++ src/components/select/Main.jsx
@@ -0,0 +1,25 @@
+import cn from "@/utils/cn";
+
+import { Ic_select } from "@/icons/Ic_arrow";
+
+const Main = ({ value, setValue, className = "", children, ...props }) => {
+  return (
+    <div className="relative max-w-min">
+      <div className="absolute right-2 top-1/2 -translate-y-1/2">
+        <Ic_select className="size-4" />
+      </div>
+      <select
+        {...props}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className={cn(
+          `h-10 px-4 w-56 cursor-pointer rounded-lg ring-blue-200 transition-all placeholder:text-slate-500 focus:ring-[3px] bg-slate-100 appearance-none ${className}`
+        )}
+      >
+        {children}
+      </select>
+    </div>
+  );
+};
+
+export default Main;
